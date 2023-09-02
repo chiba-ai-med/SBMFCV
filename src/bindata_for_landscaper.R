67f1ce4971eaed16a2952a809c4ae89414454d3e
kokitsuyuzaki <k.t.the-answer@hotmail.co.jp>
2023-09-02 10:37:24 +0900

    fix: Non-tab delimited File

--- src/bindata_for_landscaper.R
+++ src/bindata_for_landscaper.R
@@ -10,4 +10,4 @@ U[which(U <= 0.5)] <- -1
 U[which(U > 0.5)] <- 1
 
 # Save
-write.table(U, outfile, row.names=FALSE, col.names=FALSE, quote=FALSE)
+write.table(U, outfile, row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
