a96e150f4176ea897f9c9e0952cb897bfe4683c9
kokitsuyuzaki <k.t.the-answer@hotmail.co.jp>
2023-09-06 12:28:49 +0900

    fix: aggregate

--- src/aggregate_nmf.R
+++ src/aggregate_nmf.R
@@ -16,7 +16,7 @@ out <- expand.grid(trial=trial_index, rank=rank_index, value=0)
 count <- 1
 for(i in seq_along(rank_index)){
 	for(j in trial_index){
-		filename = paste0(outdir, "/nmf/", rank_index[i], "/", j, ".txt")
+		filename = paste0(outdir, "/nmf/", rank_index[i], "/", j, "_error.txt")
 		out[count, 3] <- read.table(filename)
 		count <- count + 1
 	}
--- src/aggregate_sbmf.R
+++ src/aggregate_sbmf.R
@@ -16,7 +16,7 @@ out <- expand.grid(trial=trial_index, lambda=lambda_index, value=0)
 count <- 1
 for(i in seq_along(lambda_index)){
 	for(j in trial_index){
-		filename = paste0(outdir, "/sbmf/", lambda_index[i], "/", j, ".txt")
+		filename = paste0(outdir, "/sbmf/", lambda_index[i], "/", j, "_error.txt")
 		out[count, 3] <- read.table(filename)
 		count <- count + 1
 	}
